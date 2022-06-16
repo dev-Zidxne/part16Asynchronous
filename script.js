0e709cccbf5aca3e0ccf70e9b905259f23840d57
Zidane Innis <zidaneinnis@gmail.com>
2022-06-16 05:22:18 -0500

    Fixed some bugs

--- script.js
+++ script.js
@@ -250,6 +250,8 @@ whereAmI(52.508, 13.381);
 whereAmI(19.037, 72.873);
 whereAmI(-33.933, 18.474);
 */
+// The Event Loop in Practice
+
 console.log('Test start');
 setTimeout(() => console.log('0 sec timer'), 0);
 Promise.resolve('Resolved promise 1').then(res => console.log(res));
